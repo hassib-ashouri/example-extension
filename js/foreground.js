101790c2e5b6294caee8752b14ed26595c0e8777
Hassib Ashouri <hassib291@hotmail.com>
2021-08-25 21:21:46 -0700

    inject react in right column of twitter

--- js/foreground.js
+++ js/foreground.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { render } from 'react-dom'
+import React, { createElement } from 'react';
+import { render } from 'react-dom';
+import Foreground from './components/Foreground';
 
 console.log("From foreground.");
 debugger;
 
-let sCardTemplate = `
-<div style="width:100px; height:100px;background-color: blue; position: relative; top: 25px; left: 25px;">
-    Text iside div {{name}}
-</div>
-`;
-let oData = {name: "hassib"};
-// let sCompiledCardTemplate = Handlebars.compile(sCardTemplate, {data: oData});
-// console.log("compiled tempalte", sCompiledCardTemplate);
+// const nd_referenceNode = document.querySelector("#react-root > didiv > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-zso239.r-1hycxz > div > div.css-1dbjc4n.r-gtdqiz.r-1hycxz > div > div > div > div:nth-child(3)v");
+const nd_sideColumn = document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-zso239.r-1hycxz > div > div.css-1dbjc4n.r-gtdqiz.r-1hycxz").children[0].children[0].children[0]; 
 
-var nd_sideColumn = document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-zso239.r-1hycxz");
+const nd_sideBarContainer = document.createElement("div");
+nd_sideBarContainer.style.display = "block";
+nd_sideBarContainer.style.all = "initial";
+nd_sideBarContainer.style.font = "inherit";
 
+
+if(nd_sideColumn){
+    nd_sideColumn.insertBefore(nd_sideBarContainer, nd_sideColumn.children[2]);
+    debugger;
+    console.log("element to render in", nd_sideColumn);    
+    render(<Foreground/>, nd_sideColumn.children[2]);
+}
 
 const sPageUrl = location.href;
 console.log("current  url: ", sPageUrl);
